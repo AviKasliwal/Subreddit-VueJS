9d83d131a6751675218edf6868d11c26d85f1386
Avi Kasliwal <avi.kasliwal97@gmail.com>
2018-12-19 10:26:28 +0530

    All the dev files

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# subreddit-vueJS
-a simple subreddit app using vue.js that fetches sub-reddits from its api
+# subreddit
+
+> a simple VueJS app that fetches subrredits from its api
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
+For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <header>
+      <h3>Subreddits</h3>
+    </header>
+    <Subreddits category="food"> </Subreddits>
+    <Subreddits category="sports"> </Subreddits>
+    <Subreddits category="funny"> </Subreddits>
+    <Subreddits category="pets"> </Subreddits>
+    <Subreddits category="art"> </Subreddits>
+    <Subreddits category="internet"> </Subreddits>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld'
+import Subreddits from './components/Subreddits'
+export default {
+  name: 'App',
+  components: {
+    Subreddits
+  }
+}
+</script>
+
+<style>
+  body {
+    font-family: arial;
+    color: #666666;
+    background: #f4f4f4;
+    margin: 0;
+    padding: 0;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  header {
+    background: coral;
+    color: #ffffff;
+    padding: 10px;
+    text-align: center;
+  }
+</style>
